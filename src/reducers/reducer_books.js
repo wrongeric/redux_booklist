386d05cec5a55ed2c9f6da7e082a23ce50faece5
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-01-30 12:57:35 -0800

    finished first reducer and connect, moving onto actions

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+    return [
+        {title: 'Javascript' },
+        {title: 'Harry Potter'},
+        {title: 'Eloquent Javascript'},
+        {title: 'The Dark Knight'}
+    ]
+}
+
+//step one of reduces is to create the reducer
+//step two is to wire up the reducer
