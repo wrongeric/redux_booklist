9c9287893be310ae5e17162f5a81b0f2fd37dbee
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-01-30 16:51:57 -0800

    redux_booklist

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        {title: 'Javascript' },
-        {title: 'Harry Potter'},
-        {title: 'Eloquent Javascript'},
-        {title: 'The Dark Knight'}
+        {title: 'Javascript', pages: 101 },
+        {title: 'Harry Potter', pages: 400},
+        {title: 'Eloquent Javascript', pages: 2000},
+        {title: 'The Dark Knight', pages: 12},
     ]
 }
 
